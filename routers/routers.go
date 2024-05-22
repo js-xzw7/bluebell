750f5553a7f5f62148b619554f216d9c14aceff1
jsxzw7 <jsxzw7@163.com>
2024-05-22 17:46:39 +0800

    add router

--- routers/routers.go
+++ routers/routers.go
@@ -1,6 +1,8 @@
 package routers
 
 import (
+	"bluebell/controller"
+	"bluebell/middleware/auth"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,8 +11,11 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	v1 := r.Group("/api/v1")
-	v1.POST("/login")
+	v1.POST("/login", controller.LoginHandler)
+	v1.POST("/signup", controller.SignUpHandler)
+	v1.GET("/refresh_token", controller.RefreshTokenHandler)
 
+	v1.Use(auth.JWTAuthMiddleware())
 	r.NoRoute(func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"msg": "404",
